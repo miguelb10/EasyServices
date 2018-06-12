7cd70e373269d233feeadbe247c19e995a474d9e
Miguel Blas <miguel.blas_03@hotmail.com>
2018-06-11 22:04:14 -0500

    Commit 13
    Resgistro de usuario

--- EasyServices/src/main/java/com/upc/controllers/UsuarioController.java
+++ EasyServices/src/main/java/com/upc/controllers/UsuarioController.java
@@ -1,14 +1,18 @@
 package com.upc.controllers;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.upc.entity.Usuario;
 import com.upc.service.UsuarioService;
@@ -25,9 +29,9 @@ public class UsuarioController {
 			HttpSession session, ModelMap modelMap) {
 		if(usuarioService.findByUsernameAndPassword(usuario.getUsername(), usuario.getPassword()) != null){
 			Usuario usersession = usuarioService.findByUsernameAndPassword(usuario.getUsername(), usuario.getPassword());
-			usuario.setNombre_completo(usersession.getNombre_completo());
+			usuario.setNombreCompleto(usersession.getNombreCompleto());
 			session.setAttribute("username", usuario.getUsername());
-			session.setAttribute("nombre_completo", usuario.getNombre_completo());
+			session.setAttribute("nombreCompleto", usuario.getNombreCompleto());
 			return "usuario_sesion";
 		}else{
 			modelMap.put("error", "Usuario invalido");
@@ -52,12 +56,15 @@ public class UsuarioController {
 		return "usuarios";
 	}
 	
-
 	@RequestMapping("/index_registrar")
-	public String indexRegistrar(Model model) {
-		Usuario usuario=new Usuario();
-		model.addAttribute("usuario",usuario );
+	public String newUsuario(Model model) {
+		model.addAttribute("usuario", new Usuario());
 		return "index_registrar";
+	}
+	@RequestMapping(value = "/usuarioRegistrar", method = RequestMethod.POST)
+	public String saveUsuario(Usuario usuario) {
+		usuarioService.saveUsuario(usuario);
+		return "redirect:/index";
 	}
 	
 	@RequestMapping("/usuario_perfiles")
